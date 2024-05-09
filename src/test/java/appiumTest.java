4ea77e4e8215acd5a6c504544a33fc73f5f4e0ef
OUQA RACHID <100094864+Bafia06@users.noreply.github.com>
2024-05-09 01:48:41 +0100

    Update appiumTest.java

--- src/test/java/appiumTest.java
+++ src/test/java/appiumTest.java
@@ -1,11 +1,8 @@
-import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
