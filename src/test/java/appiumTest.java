757dbdd2bc93b133886e0478c1ce4cf74a501f8e
OUQA RACHID <100094864+Bafia06@users.noreply.github.com>
2024-05-22 14:24:08 +0100

    Update appiumTest.java

--- src/test/java/appiumTest.java
+++ src/test/java/appiumTest.java
@@ -15,22 +15,22 @@ public class appiumTest {
     WebDriver driver;
     @BeforeTest
     public void init() throws MalformedURLException {
-        File app= new File("C:\\tools\\jenkins-agent\\Dropbox_374.2.4_apkcombo.com.apk");
+        File app= new File("C:\\Users\\rouqa\\Desktop\\bitbar-sample-app.apk");
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("platformName", "Android");
         capabilities.setCapability("automationName", "UiAutomator2");
         capabilities.setCapability("deviceName", "device");
         capabilities.setCapability("app", app.getAbsolutePath());
-        capabilities.setCapability("appActivity", "com.dropbox.dbapp.auth.login.DbappLoginActivity");
-        capabilities.setCapability("appPackage", "com.dropbox.android");
-        driver = new AndroidDriver(new URL("http://localhost:4723/"), capabilities);
+        capabilities.setCapability("appActivity", "com.bitbar.testdroid.BitbarSampleApplicationActivity");
+        capabilities.setCapability("appPackage", "com.bitbar.testdroid");
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"), capabilities);
 
     }
 
     @Test
     public void appiumTest(){
         //driver.findElement(By.xpath("//android.widget.TextView[@text='Sign up']")).click();
-        driver.findElement(By.xpath("//android.widget.Button")).click();
+        driver.findElement(By.xpath("//android.widget.RadioButton[@resource-id=\"com.bitbar.testdroid:id/radio2\"]")).click();
     }
 
     @AfterTest
